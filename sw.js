e9a21566144d2674098a34bff152100a5b58a22f
uPokoy <120357426+uPokoy@users.noreply.github.com>
2026-09-06 00:26:12 +0300

    Refresh cache for view settings

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'my-income-v3';
+const CACHE = 'my-income-v4';
 const ASSETS = ['./','./index.html','./styles.css','./app.js','./manifest.json'];
 
 self.addEventListener('install', event => {
